afbc4e71e740b458f92d9250c21f660d10e563dc
SPIKI\ricod <denricren@alu.edu.gva.es>
2026-02-17 14:31:31 +0100

    no message

--- Programacion_objestos/src/main/java/insti/Appinsti.java
+++ Programacion_objestos/src/main/java/insti/Appinsti.java
@@ -2,13 +2,13 @@ package insti;
 
 public class Appinsti {
     static void main(String[] args) {
-        Instituto insti = new Instituto();
+//        Instituto insti = new Instituto();
 
         Instituto enric = new Instituto("enric","Campello");
 
         System.out.println(enric);
 
-        System.out.println(insti);
+//        System.out.println(insti);
 
     }
 }
