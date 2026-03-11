b53813c8e83b0b2f56ecd24657197fd08e275edd
Denis Rico. <ricorenzdenis@gmail.com>
2026-03-11 19:30:27 +0100

    accepta el reto hecho

--- Programacion_objestos/src/main/java/Mapas/Va_de_moda.java
+++ Programacion_objestos/src/main/java/Mapas/Va_de_moda.java
@@ -13,22 +13,21 @@ public class Va_de_moda {
             return false;
         else {
             String numeros = in.nextLine();
-            HashMap<Integer,Integer>mapa = new HashMap<>();
+            HashMap<String,Integer>mapa = new HashMap<>();
             for (String num : numeros.split(" ")){
-                if (mapa.containsKey(Integer.parseInt(num))){
-                    mapa.put(Integer.parseInt(num),mapa.get(Integer.parseInt(num))+1);
+                if (mapa.containsKey(num)){
+                    mapa.put(num,mapa.get(num)+1);
                 }else {
-                    mapa.put(Integer.parseInt(num),1);
+                    mapa.put(num,1);
                 }
             }
             int mayor = 0;
             int definitivo = 0;
-            for (Map.Entry<Integer,Integer>resultado : mapa.entrySet()){
-//                if (mayor < resultado.getValue()){
-//                    mayor = resultado.getValue();
-//                    definitivo = resultado.getKey();
-//                }
-                System.out.println(resultado.getKey());
+            for (Map.Entry<String,Integer>resultado : mapa.entrySet()){
+                if (mayor < resultado.getValue()){
+                    mayor = resultado.getValue();
+                    definitivo = Integer.parseInt(resultado.getKey());
+                }
             }
             System.out.println(definitivo);
 
