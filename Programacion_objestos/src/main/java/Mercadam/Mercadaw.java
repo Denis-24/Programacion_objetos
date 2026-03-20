0a4cadfdc9030198118353e839eebc9a2ec73463
Denis Rico. <ricorenzdenis@gmail.com>
2026-03-20 17:14:40 +0100

    test diccionario

--- Programacion_objestos/src/main/java/Mercadam/Mercadaw.java
+++ Programacion_objestos/src/main/java/Mercadam/Mercadaw.java
@@ -1,5 +1,5 @@
 package Mercadam;
 
 public class Mercadaw {
-    
+
 }
---
+++ Programacion_objestos/src/test/java/Mapas/DiccionarioTest.java
@@ -0,0 +1,24 @@
+package Mapas;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiccionarioTest {
+
+    @Test
+    public void traduce() {
+
+
+    }
+
+    @Test
+    public void aleatorio() {
+
+    }
+
+    @Test
+    public void primeraLetraTraducir() {
+
+    }
+}
