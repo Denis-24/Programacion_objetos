0e875a708b59e07d6e9471ff89c701cdc6593e28
SPIKI\ricod <denricren@alu.edu.gva.es>
2026-03-24 14:35:45 +0100

    no message

--- Programacion_objestos/src/main/java/Iterators/Iteradores_objetos.java
+++ Programacion_objestos/src/main/java/Iterators/Iteradores_objetos.java
@@ -1,6 +1,7 @@
 package Iterators;
 
 import Colecciones.Presidentes;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,20 +10,20 @@ import java.util.Iterator;
 public class Iteradores_objetos {
     static void main(String[] args) {
 
-        ArrayList<Presidentes> lista = new ArrayList<>(Arrays.asList(new Presidentes("Donald Trump","USA"),
-                new Presidentes("Benja Netanyahu","ISRAEL"),
-                new Presidentes("Pedro Sanchez","España")));
-        Iterator<Presidentes> it = lista.iterator();
-
-        while (it.hasNext()){
-            Presidentes presi = it.next();
-
-            if (presi.getNombre().equals("Benja")){
-                it.remove();
-            }
-        }
-        System.out.println(lista);
-
+//        ArrayList<Presidentes> lista = new ArrayList<>(Arrays.asList(new Presidentes("Donald Trump","USA"),
+//                new Presidentes("Benja Netanyahu","ISRAEL"),
+//                new Presidentes("Pedro Sanchez","España")));
+//        Iterator<Presidentes> it = lista.iterator();
+//
+//        while (it.hasNext()){
+//            Presidentes presi = it.next();
+//
+//            if (presi.getNombre().equals("Benja")){
+//                it.remove();
+//            }
+//        }
+//        System.out.println(lista);
+//
 
 
 
